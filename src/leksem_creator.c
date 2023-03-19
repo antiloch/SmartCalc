c363819b508f250ec21e0c7b9658853baec0198a
Antilochus Spring <antiloch@il-c4.msk.21-school.ru>
2023-03-19 15:49:42 +0300

    fix

--- src/leksem_creator.c
+++ src/leksem_creator.c
@@ -1,72 +1,80 @@
 #include "leksem_creator.h"
 
-int converter_to_lexeme(char **input_str, struct lexeme *output_lexemes)
-{
-    int result = FAIL;
-    while (**input_str != '\0')
-    {
-        if (handle_number(input_str, output_lexemes)) result = SUCCESS;
-        else if (handle_operator(input_str, output_lexemes)) result = SUCCESS;
-        else result = FAIL;
-        output_lexemes = output_lexemes + 1;
-    }
-    return result;
-}
-
-int handle_number(char **input_str, struct lexeme *output_lexeme)
-{
-    int result = FAIL;
-    if (**input_str <= '9' && **input_str >= '0')
-    {
-        add_number(input_str, output_lexeme); 
-        result = SUCCESS;
-    }
-    return result;
-}
-
-int handle_operator(char **input_str, struct lexeme *output_lexeme)
-{   
-    int result = SUCCESS;
-    output_lexeme->value_type = OPERATOR;
-    if (**input_str == '(') output_lexeme->value = '(';
-    else if (**input_str == ')') output_lexeme->value = ')';
-    else if (**input_str == '*') output_lexeme->value = '*';
-    else if (**input_str == '-') output_lexeme->value = '-';
-    else if (**input_str == '+') output_lexeme->value = '+';
-    else if (**input_str == '/') output_lexeme->value = '/';
-    else if (**input_str == '^') output_lexeme->value = '^';
-    else result = FAIL;
-    *input_str = *input_str + 1;
-    return result;
-}
-
-void add_number(char **input_str, struct lexeme *output_lexeme)
-{
-    int int_number = converter_str_to_int(input_str), int_float = 0;
-    if (**input_str == '.') {
-        *input_str = *input_str + 1;
-        int_float = converter_str_to_int(input_str);
-    }
-    if (int_float == 0)
-    {
-        output_lexeme->value = int_number;
-        output_lexeme->value_type = INT_NUMBER;
-    }
+int converter_to_lexeme(char **input_str, struct lexeme *output_lexemes) {
+  int result = SUCCESS;
+  while (**input_str != '\0' && result != FAIL) {
+    if (handle_number(input_str, output_lexemes))
+      ;
+    else if (handle_operator(input_str, output_lexemes))
+      ;
     else
-    {
-        int nDigits = floor(log10(int_float)) + 1;
-        output_lexeme->double_num = int_number + int_float * pow(10, -nDigits);
-        output_lexeme->value_type = FLOAT_NUMBER;
-    }
-    
+      result = FAIL;
+    output_lexemes = output_lexemes + 1;
+  }
+  return result;
 }
 
-int converter_str_to_int(char **input_str)
-{
-    char *str_number = calloc(255, sizeof(char));
-    strncpy(str_number, *input_str, strspn(*input_str, "0123456789"));
-    *input_str = *input_str + strspn(*input_str, "0123456789");
-    int result = atoi(str_number);
-    free(str_number);
-    return result;
+int handle_number(char **input_str, struct lexeme *output_lexeme) {
+  int result = SUCCESS;
+  if (**input_str <= '9' && **input_str >= '0')
+    add_number(input_str, output_lexeme);
+  else
+    result = FAIL;
+  return result;
+}
+
+int handle_operator(char **input_str, struct lexeme *output_lexeme) {
+  int result = SUCCESS;
+  output_lexeme->value_type = OPERATOR;
+  if (**input_str == '(')
+    output_lexeme->value = '(';
+  else if (**input_str == ')')
+    output_lexeme->value = ')';
+  else if (**input_str == '*')
+    output_lexeme->value = '*';
+  else if (**input_str == '-')
+    output_lexeme->value = '-';
+  else if (**input_str == '+')
+    output_lexeme->value = '+';
+  else if (**input_str == '/')
+    output_lexeme->value = '/';
+  else if (**input_str == '^')
+    output_lexeme->value = '^';
+  else
+    result = FAIL;
+  *input_str = *input_str + 1;
+  return result;
+}
+
+void add_number(char **input_str, struct lexeme *output_lexeme) {
+  int int_number = 0, int_float = 0;
+  read_number(input_str, &int_number, &int_float);
+  write_number(output_lexeme, int_number, int_float);
+}
+
+void read_number(char **input_str, int *int_number, int *int_float) {
+  *int_number = converter_str_to_int(input_str), *int_float = 0;
+  if (**input_str == '.') {
+    *input_str = *input_str + 1;
+    *int_float = converter_str_to_int(input_str);
+  }
+}
+
+void write_number(lexeme *output_lexeme, int int_number, int int_float) {
+  output_lexeme->value = int_number;
+  output_lexeme->value_type = INT_NUMBER;
+  if (int_float != 0) {
+    output_lexeme->double_num =
+        int_number + int_float * pow(10, -(floor(log10(int_float)) + 1));
+    output_lexeme->value_type = FLOAT_NUMBER;
+  }
+}
+
+int converter_str_to_int(char **input_str) {
+  char *str_number = calloc(255, sizeof(char));
+  strncpy(str_number, *input_str, strspn(*input_str, "0123456789"));
+  *input_str = *input_str + strspn(*input_str, "0123456789");
+  int result = atoi(str_number);
+  free(str_number);
+  return result;
 }
--- src/leksem_creator.h
+++ src/leksem_creator.h
@@ -1,31 +1,31 @@
 #ifndef LEXEME_CREATOR_H_
 #define LEXEME_CREATOR_H_
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
 #include <math.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define INT_NUMBER 1
 #define FLOAT_NUMBER 2
 #define OPERATOR 3
-#define END 0 
+#define END 0
 #define SUCCESS 1
 #define FAIL 0
 
-typedef struct lexeme
-{
-    int value;
-    double double_num;
-    int value_type;
+typedef struct lexeme {
+  int value;
+  double double_num;
+  int value_type;
 } lexeme;
 
 int converter_to_lexeme(char **input_str, struct lexeme *output_lexemes);
 int handle_number(char **input_str, struct lexeme *output_lexeme);
 int handle_operator(char **input_str, struct lexeme *output_lexeme);
 void add_number(char **input_str, struct lexeme *output_lexeme);
+void read_number(char **input_str, int *int_number, int *int_float);
+void write_number(struct lexeme *output_lexeme, int int_number, int int_float);
 int converter_str_to_int(char **input_str);
 
-
-#endif // LEXEME_CREATOR_H_
+#endif  // LEXEME_CREATOR_H_
