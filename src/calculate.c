dc7ebd71d8873285fc40ee21034f1cb3e916a74e
Antilochus Spring <antiloch@at-m7.msk.21-school.ru>
2023-04-01 15:06:20 +0300

    working () && dificult functions

--- src/calculate.c
+++ src/calculate.c
@@ -31,6 +31,64 @@ double stack_calculations(lexeme *input_lexemes, double variable)
                 result = pop_num(&stack) - result;
                 push_num(&stack, result);
             }
+            else if (input_lexemes->value == '*') {
+                result = pop_num(&stack) * pop_num(&stack);
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == '/') {
+                result = pop_num(&stack);
+                result = pop_num(&stack) / result;
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == '^') {
+                result = pop_num(&stack);
+                result = pow(pop_num(&stack), result);
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == LN) {
+                result = log(pop_num(&stack));
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == LG) {
+                result = log10(pop_num(&stack));
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == SIN) {
+                result = sin(pop_num(&stack));
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == COS) {
+                result = cos(pop_num(&stack));
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == TAN) {
+                result = tan(pop_num(&stack));
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == ASIN) {
+                result = asin(pop_num(&stack));
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == ACOS) {
+                result = acos(pop_num(&stack));
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == ATAN) {
+                result = atan(pop_num(&stack));
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == SQRT) {
+                result = sqrt(pop_num(&stack));
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == UNAR_MINUS) {
+                result = -pop_num(&stack);
+                push_num(&stack, result);
+            }
+            else if (input_lexemes->value == UNAR_PLUS) {
+                result = pop_num(&stack);
+                push_num(&stack, result);
+            }
         }
         input_lexemes = input_lexemes + 1;
         printf("HERE, %lf\n", get_first_number(&stack));
--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 
 int main() {
   int result = SUCCESS;
-  char *example = "20-1+1+1.4";
+  char *example = "3*(-2+1)";
   calculate(example);
   return !result;
 }
