ddb6731fadb975b051a6b37d57de1e433578e331
Antilochus Spring <antiloch@il-c4.msk.21-school.ru>
2023-05-14 13:55:14 +0300

    new tests

--- src/tests/test_lexeme.c
+++ src/tests/test_lexeme.c
@@ -1,23 +1,67 @@
 #include "test_main.h"
 
-START_TEST(simple_calculate1) {
+START_TEST(sum) {
   char *test = "3+1";
   double result = calculate(test);
   ck_assert_double_eq(result, 4);
 }
 END_TEST
 
-START_TEST(simple_calculate2) {
+START_TEST(multi_sum) {
   char *test = "10+800+90+78+83+93.3+211921.42";
   double result = calculate(test);
   ck_assert_double_eq_tol(result, 213075.72, 1e-6);
 }
 END_TEST
 
-START_TEST(simple_calculate3) {
+START_TEST(one_number) {
+  char *test = "211921.42";
+  double result = calculate(test);
+  ck_assert_double_eq_tol(result, 211921.42, 1e-6);
+}
+END_TEST
+
+START_TEST(lg_ln_sin_cos_tan_sqrt) {
   char *test = "lg(-ln(sin(cos(tan(sqrt(1))))))";
   double result = calculate(test);
   ck_assert_double_eq_tol(result, 0.6348208, 1e-6);
+}
+END_TEST
+
+START_TEST(arcsin) {
+  char *test = "asin(0.0)";
+  double result = calculate(test);
+  ck_assert_double_eq_tol(result, 0, 1e-6);
+  char *test2 = "asin(0.3)";
+  result = calculate(test2);
+  ck_assert_double_eq_tol(result, 0.304693, 1e-6);
+}
+END_TEST
+
+START_TEST(arccos) {
+  char *test = "acos(1)";
+  double result = calculate(test);
+  ck_assert_double_eq_tol(result, 0, 1e-6);
+  char *test2 = "acos(0.3)";
+  result = calculate(test2);
+  ck_assert_double_eq_tol(result, 1.266104, 1e-6);
+}
+END_TEST
+
+START_TEST(arctan) {
+  char *test = "atan(1)";
+  double result = calculate(test);
+  ck_assert_double_eq_tol(result, 0.785398, 1e-6);
+  char *test2 = "atan(0.3)";
+  result = calculate(test2);
+  ck_assert_double_eq_tol(result, 0.291457, 1e-6);
+}
+END_TEST
+
+START_TEST(nan_calculations) {
+  char *test = "0";
+  double result = calculate(test);
+  ck_assert_double_eq_tol(result, 0, 1e-6);
 }
 END_TEST
 
@@ -26,9 +70,14 @@ Suite *test_lexeme(void) {
   TCase *tc = tcase_create("tc_+");
 
   suite_add_tcase(s, tc);
-  tcase_add_test(tc, simple_calculate1);
-  tcase_add_test(tc, simple_calculate2);
-  tcase_add_test(tc, simple_calculate3);
+  tcase_add_test(tc, sum);
+  tcase_add_test(tc, multi_sum);
+  tcase_add_test(tc, one_number);
+  tcase_add_test(tc, lg_ln_sin_cos_tan_sqrt);
+  tcase_add_test(tc, arcsin);
+  tcase_add_test(tc, arccos);
+  tcase_add_test(tc, arctan);
+  tcase_add_test(tc, nan_calculations);
 
   suite_add_tcase(s, tc);
   return s;
