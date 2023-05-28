356769e995ea1ba0190e40932d2bca29da6baac2
Тимофей Щенников <antiloch@MacBook-Air-Timofej.local>
2023-05-28 18:13:11 +0300

    add tests

--- src/tests/test_errors.c
+++ src/tests/test_errors.c
@@ -79,6 +79,17 @@ START_TEST(error_blank_request) {
 }
 END_TEST
 
+START_TEST(error_less_numbers) {
+  char *test = "2*";
+  double result = 0;
+  int status = calculate(test, &result);
+  ck_assert_int_eq(status, FAIL);
+  char *test2 = "mod4";
+  status = calculate(test2, &result);
+  ck_assert_int_eq(status, FAIL);
+}
+END_TEST
+
 Suite *test_errors(void) {
   Suite *s = suite_create("\033[1;33m Error tests \033[0m");
   TCase *tc = tcase_create("tc_E");
@@ -92,6 +103,7 @@ Suite *test_errors(void) {
   tcase_add_test(tc, error_log_less_zero);
   tcase_add_test(tc, error_no_such_function);
   tcase_add_test(tc, error_blank_request);
+  tcase_add_test(tc, error_less_numbers);
 
   suite_add_tcase(s, tc);
   return s;
